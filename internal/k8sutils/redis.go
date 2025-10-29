889184528ceb251f8ad677836dca9ab1c2defcb5
muicoder <muicoder@gmail.com>
2025-10-29 17:28:33 +0800

    AIO for build

--- internal/k8sutils/redis.go
+++ internal/k8sutils/redis.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 	"time"
 
-	commonapi "github.com/OT-CONTAINER-KIT/redis-operator/api/common/v1beta2"
+	"github.com/OT-CONTAINER-KIT/redis-operator/api/common/v1beta2"
 	rcvb2 "github.com/OT-CONTAINER-KIT/redis-operator/api/rediscluster/v1beta2"
 	rrvb2 "github.com/OT-CONTAINER-KIT/redis-operator/api/redisreplication/v1beta2"
 	common "github.com/OT-CONTAINER-KIT/redis-operator/internal/controller/common"
@@ -47,7 +47,6 @@ func getRedisServerIP(ctx context.Context, client kubernetes.Interface, redisInf
 
 	redisPod, err := client.CoreV1().Pods(redisInfo.Namespace).Get(context.TODO(), redisInfo.PodName, metav1.GetOptions{})
 	if err != nil {
-		log.FromContext(ctx).Error(err, "Error in getting Redis pod IP", "namespace", redisInfo.Namespace, "podName", redisInfo.PodName)
 		return ""
 	}
 
@@ -260,7 +259,7 @@ func ExecuteRedisClusterCommand(ctx context.Context, client kubernetes.Interface
 	executeCommand(ctx, client, cr, cmd.Args(), cr.Name+"-leader-0")
 }
 
-func getRedisTLSArgs(tlsConfig *commonapi.TLSConfig, clientHost string) []string {
+func getRedisTLSArgs(tlsConfig *v1beta2.TLSConfig, clientHost string) []string {
 	cmd := []string{}
 	if tlsConfig != nil {
 		cmd = append(cmd, "--tls")
@@ -632,7 +631,7 @@ func getContainerID(ctx context.Context, client kubernetes.Interface, cr *rcvb2.
 	targetContainer := -1
 	for containerID, tr := range pod.Spec.Containers {
 		log.FromContext(ctx).V(1).Info("Inspecting container", "Pod Name", podName, "Container ID", containerID, "Container Name", tr.Name)
-		if tr.Name == cr.Name+"-leader" {
+		if tr.Name == redisContainer {
 			targetContainer = containerID
 			log.FromContext(ctx).V(1).Info("Leader container found", "Container ID", containerID, "Container Name", tr.Name)
 			break
@@ -727,7 +726,6 @@ func GetRedisNodesByRole(ctx context.Context, cl kubernetes.Interface, cr *rrvb2
 func checkRedisServerRole(ctx context.Context, redisClient *redis.Client, podName string) (string, error) {
 	info, err := redisClient.Info(ctx, "Replication").Result()
 	if err != nil {
-		log.FromContext(ctx).Error(err, "Failed to Get the role Info of the", "redis pod", podName)
 		return "", err
 	}
 	lines := strings.Split(info, "\r\n")
